243927bfc388a53c94e05a66ad356e939745789f
Fernando Cesar Balbino <fernando.balbino@ifms.edu.br>
2026-09-11 20:40:50 -0400

    Ajuste do arquivo application.properties para carregar arquivo .env

--- src/main/java/br/edu/ifms/cinema/service/MovieService.java
+++ src/main/java/br/edu/ifms/cinema/service/MovieService.java
@@ -3,7 +3,6 @@ package br.edu.ifms.cinema.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import br.edu.ifms.cinema.model.MovieResults;
